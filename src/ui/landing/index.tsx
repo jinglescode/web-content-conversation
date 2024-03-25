5f1430f1f739d372e41cb61aa80620e7f857ae0d
Hong Jing (Jingles) <jinglescode@gmail.com>
2024-03-25 10:49:33 +0800

    update icon and landing page

--- src/ui/landing/index.tsx
+++ src/ui/landing/index.tsx
@@ -1,5 +1,25 @@
+import Demo from "./Demo";
+import Header from "./Header";
 import Hero from "./Hero";
 
 export default function UiLanding() {
-  return <Hero />;
+  return (
+    <div
+      className="h-screen flex flex-col items-center"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "0px",
+        gap: "64px",
+
+        background: "#0066FF",
+        borderRadius: "0px",
+      }}
+    >
+      <Header />
+      <Hero />
+      <Demo />
+    </div>
+  );
 }
